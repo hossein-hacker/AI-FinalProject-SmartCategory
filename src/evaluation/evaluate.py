0b362fc8b927daad13d7b4d918ab1e6bf58740fb
Amirreza-Pakrooh <amirreza.pakrooh@gmail.com>
2026-02-12 17:50:29 +0330

    Defined evaluation method

--- src/evaluation/evaluate.py
+++ src/evaluation/evaluate.py
@@ -11,9 +11,12 @@ model_path = ''
 test_dir = '' 
 
 test_transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
 ])
 
 test_dataset = datasets.ImageFolder(test_dir, transform=test_transform)
